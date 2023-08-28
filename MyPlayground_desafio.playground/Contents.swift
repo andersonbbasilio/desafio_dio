80ad99e1787db58af0cedbf5542da7ffc0caaa54
Anderson Badaró <a_badaro@hotmail.com>
2023-08-28 15:33:05 -0300

    update

--- MyPlayground_desafio.playground/Contents.swift
+++ MyPlayground_desafio.playground/Contents.swift
@@ -1,3 +1,14 @@
-import UIKit
-
-var greeting = "Hello, playground"
+            //declaração constante string
+            let name: String = "Steve"
+            //declaração variavel String optional
+            var secondName: String? = "Jobs"
+            secondName = "Wozniak"
+            
+            //saida constante name e variavel secondName
+            print("\(name) e \(secondName), foram os fundadores da Apple.")
+            
+            //optional binding na variavel
+            if let newName = secondName {
+                Text("O \(newName), foi um genial criador da placa mãe do primeiro Apple Computer.")
+            } else {
+                Text("\(secondName) não foi o único fundador da Apple.")
